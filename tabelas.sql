6d95dcaca49d5edae310b929200d0a70b5e1d5d2
André_ats <100627370+Andre-ats@users.noreply.github.com>
2024-05-21 18:07:31 -0300

    Ajuste de relações entre tabelas com base no diagrama

--- tabelas.sql
+++ tabelas.sql
@@ -1,40 +1,68 @@
-CREATE TABLE cursos (
-	id int NOT NULL AUTO_INCREMENT,
-	nome varchar(255),
-	semestres int NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE professores (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    email varchar(255),
+    salario decimal(15, 2),
+    departamento_id int NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE disciplina (
-	id int NOT NULL AUTO_INCREMENT,
-	nome varchar(255),
-	PRIMARY KEY (id)
+CREATE TABLE departamento (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    chefe_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (chefe_id) REFERENCES professores(id)
+);
+
+CREATE TABLE cursos (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    semestres int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE disciplinas (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    curso_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (curso_id) REFERENCES cursos(id)
 );
 
 CREATE TABLE alunos (
-	id int NOT NULL AUTO_INCREMENT,
-	nome varchar(255),
-	ra varchar(15),
-	curso int NOT NULL,
-	email varchar(255),
-	PRIMARY KEY (id),
-	FOREIGN KEY (curso) REFERENCES cursos(id)
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    email varchar(255),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE professores (
-	id int NOT NULL AUTO_INCREMENT,
-	nome varchar(255),
-	email varchar(255),
-	salario decimal(15,2),
-	PRIMARY KEY (id)
+CREATE TABLE alunos_cursou (
+    aluno_id int NOT NULL,
+    disciplina_id int NOT NULL,
+    semestre int NOT NULL,
+    ano int NOT NULL,
+    nota int,
+    FOREIGN KEY (aluno_id) REFERENCES alunos(id),
+    FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
 );
 
-CREATE TABLE departamentos (
-	id int NOT NULL AUTO_INCREMENT,
-	nome varchar(255),
-	chefe_departamento int NOT NULL,
-	curso int NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (chefe_departamento) REFERENCES professores(id),
-	FOREIGN KEY (curso) REFERENCES cursos(id)
+CREATE TABLE disciplinas_ministradas (
+    professor_id int NOT NULL,
+    disciplina_id int NOT NULL,
+    semestre int NOT NULL,
+    ano int NOT NULL,
+    FOREIGN KEY (professor_id) REFERENCES professores(id),
+    FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
 );
+
+CREATE TABLE alunos_tcc (
+    aluno_id int NOT NULL,
+    orientador_id int NOT NULL,
+    grupo int,
+    FOREIGN KEY (orientador_id) REFERENCES professores(id)
+);
+
+ALTER TABLE professores
+ADD CONSTRAINT fk_professores_departamento FOREIGN KEY (departamento_id)
+REFERENCES departamento(id);
