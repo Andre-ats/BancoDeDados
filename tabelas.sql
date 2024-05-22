4cd41cd522990243ae338daed426452e6b7fe1ef
André_ats <100627370+Andre-ats@users.noreply.github.com>
2024-05-22 16:34:38 -0300

    Colocando serial para o incremento do id

--- tabelas.sql
+++ tabelas.sql
@@ -1,68 +1,63 @@
 CREATE TABLE professores (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(255),
-    email varchar(255),
-    salario decimal(15, 2),
-    departamento_id int NOT NULL,
-    PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    salario DECIMAL(15, 2),
+    departamento_id INT NOT NULL
 );
 
 CREATE TABLE departamentos (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(255),
-    chefe_id int,
-    PRIMARY KEY (id),
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    chefe_id INT,
     FOREIGN KEY (chefe_id) REFERENCES professores(id)
 );
 
 CREATE TABLE cursos (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(255),
-    semestres int NOT NULL,
-    PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    semestres INT NOT NULL
 );
 
 CREATE TABLE disciplinas (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(255),
-    curso_id int NOT NULL,
-    PRIMARY KEY (id),
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    curso_id INT NOT NULL,
     FOREIGN KEY (curso_id) REFERENCES cursos(id)
 );
 
 CREATE TABLE alunos (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(255),
-    email varchar(255),
-    PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255)
 );
 
 CREATE TABLE alunos_cursou (
-    aluno_id int NOT NULL,
-    disciplina_id int NOT NULL,
-    semestre int NOT NULL,
-    ano int NOT NULL,
-    nota int,
+    aluno_id INT NOT NULL,
+    disciplina_id INT NOT NULL,
+    semestre INT NOT NULL,
+    ano INT NOT NULL,
+    nota INT,
     FOREIGN KEY (aluno_id) REFERENCES alunos(id),
     FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
 );
 
 CREATE TABLE disciplinas_ministradas (
-    professor_id int NOT NULL,
-    disciplina_id int NOT NULL,
-    semestre int NOT NULL,
-    ano int NOT NULL,
+    professor_id INT NOT NULL,
+    disciplina_id INT NOT NULL,
+    semestre INT NOT NULL,
+    ano INT NOT NULL,
     FOREIGN KEY (professor_id) REFERENCES professores(id),
     FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
 );
 
 CREATE TABLE alunos_tcc (
-    aluno_id int NOT NULL,
-    orientador_id int NOT NULL,
-    grupo int,
+    aluno_id INT NOT NULL,
+    orientador_id INT NOT NULL,
+    grupo INT,
     FOREIGN KEY (orientador_id) REFERENCES professores(id)
 );
 
 ALTER TABLE professores
 ADD CONSTRAINT fk_professores_departamento FOREIGN KEY (departamento_id)
-REFERENCES departamento(id);
+REFERENCES departamentos(id);
